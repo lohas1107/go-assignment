cf6c59ca0d445dc69c5ebcd83488740c7793abcf
Irene Wang <lohas1107@gmail.com>
2023-11-30 11:36:43 +0800

    feat(Q6): hello world api

--- 6_tinder/cmd/matching/router/router.go
+++ 6_tinder/cmd/matching/router/router.go
@@ -1,8 +1,16 @@
 package router
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
 
 func SetUp() *gin.Engine {
 	router := gin.Default()
+	router.GET("/", Greet)
 	return router
+}
+
+func Greet(context *gin.Context) {
+	context.JSON(http.StatusOK, "hello, world")
 }
